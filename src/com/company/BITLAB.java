54305fc010ede55b6509384ca289d231ad2f1d46
Tair <62845840+Tako-tair@users.noreply.github.com>
2021-10-27 00:24:40 +0600

    hey i changed some places

--- src/com/company/BITLAB.java
+++ src/com/company/BITLAB.java
@@ -1,5 +1,9 @@
 package com.company;
 
-public class BITLAB {
+import javax.crypto.spec.PSource;
 
+public class BITLAB {
+    public static void main(String[] args) {
+        System.out.println("hey i did it");
+    }
 }
