263b7dee6a770062f7932ce710f5fbaff6bac267
Tair <62845840+Tako-tair@users.noreply.github.com>
2021-10-27 19:16:09 +0600

    hey

--- src/com/company/BITLAB.java
+++ src/com/company/BITLAB.java
@@ -4,7 +4,7 @@ import javax.crypto.spec.PSource;
 
 public class BITLAB {
     public static void main(String[] args) {
-        System.out.println("hey i did it");
+        System.out.println("hey i did it dfv");
         System.out.println("dsdfbdfbvsdv");
     }
 }
